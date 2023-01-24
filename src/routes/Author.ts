75804ab56bff54bdfb4c4383a38cca114148f51e
tom-kotlar <t.kotlar@yahoo.com>
2023-01-24 17:26:18 +0000

    Build(routes/*): Routes

---
+++ src/routes/Author.ts
@@ -0,0 +1,15 @@
+import express, { Router } from 'express';
+import controller from '../controllers/Author';
+import { Schemas, ValidateSchema } from '../middleware/ValidateSchema';
+
+const router: Router = express.Router();
+
+router.post('/create', ValidateSchema(Schemas.author.create), controller.createAuthor);
+router.get('/get/:authorId', controller.readAuthor);
+router.get('/get', controller.readAll);
+router.patch('/update/:authorId',ValidateSchema(Schemas.author.update), controller.updateAuthor);
+router.delete('/delete/:authorId', controller.deleteAuthor);
+
+export = router;
+
+
